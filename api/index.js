92c1e6922db0c56624e36066efc956e438231f1b
Amik <amikc1996@gmail.com>
2023-05-02 16:22:16 -0400

    Added the MongoDB URL to a new .env file. Need to install dotenv to load that variable.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(express.json());
@@ -8,6 +9,9 @@ app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 }));
+
+
+mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req, res) => {
     res.json('test ok');
