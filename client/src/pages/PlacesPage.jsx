c8fa55ac4389d4ec0e4bff87d85393d300a81f02
Amik <amikc1996@gmail.com>
2023-05-15 22:05:15 -0400

    There's an error, the pictures don't show up on the site after selecting from the uploads button

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -49,13 +49,16 @@ export default function PlacesPage() {
     function uploadPhoto(ev) {
         const files = ev.target.files;
         const data = new FormData();
-        data.set('photos', files);
+        for (let i = 0; i < files.length; i++) {
+            data.append ('photos', files[i]);
+        }
+        // data.set('photos', files);
         axios.post('/upload', data, {
             headers: { 'Content-type': 'multipart/form-data' }
         }).then(response => {
-            const { data: filename } = response;
+            const { data: filenames } = response;
             setAddedPhotos(prev => {
-                return [...prev, filename];
+                return [...prev, ...filenames];
             });
         })
     }
@@ -94,12 +97,12 @@ export default function PlacesPage() {
 
                         <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
                             {addedPhotos.length > 0 && addedPhotos.map(link => (
-                                <div>
+                                <div key={link}>
                                     <img className="rounded-3xl" src={'http://localhost:4000/uploads/' + link} alt="" />
                                 </div>
                             ))}
                             <label className="cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-700">
-                                <input type="file" className="hidden" onChange={uploadPhoto} />
+                                <input type="file" multiple className="hidden" onChange={uploadPhoto} />
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                                 </svg>
