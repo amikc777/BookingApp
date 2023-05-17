5ef162d3fa8cca4f657aa73dd980170803d2bd01
Amik <amikc1996@gmail.com>
2023-05-16 21:18:38 -0400

    Fixed border sizing of uploaded pictures. Also got the Perks.jsx to work by implementing a function to handle when the check boxes are clicked.

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -97,12 +97,11 @@ export default function PlacesPage() {
 
                         <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
                             {addedPhotos.length > 0 && addedPhotos.map(link => (
-                                <div key={link}>
-                                    <img className="rounded-3xl" src={`http://localhost:4000/uploads/${link.replace('/', '\\')}`} alt="" />
-
+                                <div className="h-32 flex" key={link}>
+                                    <img className="rounded-3xl w-full object-cover" src={'http://localhost:4000/uploads/' + link} alt="" />
                                 </div>
                             ))}
-                            <label className="cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-700">
+                            <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-700">
                                 <input type="file" multiple className="hidden" onChange={uploadPhoto} />
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
