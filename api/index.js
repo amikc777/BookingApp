ae72f9deff77daf9929dee18185915208cdc682c
Amik <amikc1996@gmail.com>
2023-06-08 18:32:02 -0400

    Index page now displays the different booking options available to the user. Price has also been added. Formatting has also been done in terms of margins,etc.

--- api/index.js
+++ api/index.js
@@ -121,13 +121,13 @@ app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
 app.post('/places', (req, res) => {
     const { token } = req.cookies;
     const {
-        title, address, addedPhotos, description,
+        title, address, addedPhotos, description, price,
         perks, extraInfo, checkIn, CheckOut, maxGuests,
     } = req.body;
     jwt.verify(token, jwtSecretString, {}, async (err, userData) => {
         if (err) throw err;
         const placeDoc = await Place.create({
-            owner: userData.id,
+            owner: userData.id, price,
             title, address, photos: addedPhotos, description,
             perks, extraInfo, checkIn, CheckOut, maxGuests,
         });
@@ -136,7 +136,7 @@ app.post('/places', (req, res) => {
 
 });
 
-app.get('/places', (req, res) => {
+app.get('/user-places', (req, res) => {
     const { token } = req.cookies;
     jwt.verify(token, jwtSecretString, {}, async (err, userData) => {
         const { id } = userData;
@@ -154,7 +154,7 @@ app.put('/places', async (req, res) => {
     const { token } = req.cookies;
     const {
         id, title, address, addedPhotos, description,
-        perks, extraInfo, checkIn, CheckOut, maxGuests,
+        perks, extraInfo, checkIn, CheckOut, maxGuests, price,
     } = req.body;
 
     jwt.verify(token, jwtSecretString, {}, async (err, userData) => {
@@ -163,14 +163,19 @@ app.put('/places', async (req, res) => {
         // console.log(userData.id);
         // console.log(placeDoc.owner.toString());
         if (userData.id === placeDoc.owner.toString()) {
+            // console.log({price});
             placeDoc.set({
                 title, address, photos: addedPhotos, description,
-                perks, extraInfo, checkIn, CheckOut, maxGuests,
+                perks, extraInfo, checkIn, CheckOut, maxGuests, price,
             });
             await placeDoc.save();
             res.json('ok');
         }
     });
 });
+
+app.get('/places', async (req, res) => {
+    res.json(await Place.find());
+})
 
 app.listen(4000);
--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -17,6 +17,7 @@ export default function PlacesFormPage() {
     const [checkIn, setCheckIn] = useState('');
     const [checkOut, setCheckOut] = useState('');
     const [maxGuests, setMaxGuests] = useState(1);
+    const [price, setPrice] = useState(100);
     const [redirect, setRedirect] = useState(false);
 
     useEffect(() => {
@@ -34,6 +35,7 @@ export default function PlacesFormPage() {
             setCheckIn(data.checkIn);
             setCheckOut(data.checkOut);
             setMaxGuests(data.maxGuests);
+            setPrice(data.price);
         });
     }, [id])
 
@@ -63,7 +65,7 @@ export default function PlacesFormPage() {
         const placeData = {
             title, address, addedPhotos,
             description, perks, extraInfo,
-            checkIn, checkOut, maxGuests
+            checkIn, checkOut, maxGuests, price,
         };
         if (id) {
             // update
@@ -71,7 +73,7 @@ export default function PlacesFormPage() {
                 id, ...placeData
             });
             setRedirect(true);
-            
+
         } else {
             // new place
             await axios.post('/places', placeData);
@@ -109,7 +111,7 @@ export default function PlacesFormPage() {
                 <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
 
                 {beforeInput('Check-In/Check-Out Times', 'Add times. Remember to leave some time in between for cleaning up the room for the next guests.')}
-                <div className="grid gap-2 sm:grid-cols-3">
+                <div className="grid gap-2 grid-cols-2 md:grid-cols-4">
                     <div>
                         <h3 className="mt-2 -mb-1">Check In Time</h3>
                         <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)}
@@ -124,6 +126,11 @@ export default function PlacesFormPage() {
                     <div>
                         <h3 className="mt-2 -mb-1">Max Num Of Guests</h3>
                         <input type="number" value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} />
+                    </div>
+
+                    <div>
+                        <h3 className="mt-2 -mb-1">Price Per Night</h3>
+                        <input type="number" value={price} onChange={ev => setPrice(ev.target.value)} />
                     </div>
                 </div>
                 <div>
