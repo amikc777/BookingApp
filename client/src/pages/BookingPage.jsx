ff76839eb1973420546adc4ec948a10b85c530c5
Amik <amikc1996@gmail.com>
2023-06-17 18:57:27 -0400

    Fixed a bug that made grey space when maximizing screen on the PlacesPage. Added comments as well. Will continue to add documentation to the project.

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -5,24 +5,35 @@ import AddressLink from "../AddressLink";
 import PlaceGallery from "../PlaceGallery";
 import BookingDates from "../BookingDates";
 
+// Component for displaying the booking page
 export default function BookingPage() {
+    // Retrieve the 'id' parameter from the URL
     const { id } = useParams();
+
+    // State variable to store the booking data
     const [booking, setBooking] = useState(null);
+
+    // Fetch the booking data when the component mounts or when the 'id' parameter changes
     useEffect(() => {
         if (id) {
+            // Send a GET request to retrieve the bookings
             axios.get('/bookings').then(response => {
+                // Find the booking with the matching 'id'
                 const foundBooking = response.data.find(({ _id }) => _id === id);
                 if (foundBooking) {
+                    // Update the state with the found booking
                     setBooking(foundBooking);
                 }
             });
         }
     }, [id]);
 
+    // If the booking data is not available yet, render an empty string
     if (!booking) {
         return '';
     }
 
+    // Render the booking information and details
     return (
         <div className="my-8">
             <h1 className="text-3xl">{booking.place.title}</h1>
@@ -37,7 +48,6 @@ export default function BookingPage() {
                     <div>Total Price</div>
                     <div className="text-3xl">${booking.price}</div>
                 </div>
-
             </div>
             <PlaceGallery place={booking.place} />
         </div>
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -27,8 +27,8 @@ export default function PlacesPage() {
             <div className="mt-4">
                 {places.length > 0 && places.map(place => (
                     <Link to={'/account/places/' + place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
-                        <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
-                            <PlaceImg place={place} />
+                        <div className="flex w-32 h-32 bg-gray-300 overflow-hidden">
+                            <PlaceImg place={place} className="object-cover h-full" />
                         </div>
                         <div className="grow-0 shrink">
                             <h2 className="text-xl">{place.title}</h2>
